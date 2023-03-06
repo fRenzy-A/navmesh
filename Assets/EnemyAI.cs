f965caafa31902aa10935cfa148a85d96dfbd3a6
Nathan A <fRenzyA05@gmail.com>
2023-03-06 15:50:28 -0400

    Added first instance of AI - only pursuit and patrol

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -5,15 +5,83 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    }   
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer)    ;
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+    }
+    private void SearchWalkPoint()
+    {
+        //Calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 }
---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform m_Camera;
+
+    [Header("Movement")]
+    public float movespeed = 6f;
+    public float crouchmovespeed = 2f;
+    public float movementMultiplier = 10f;
+    public float dashstrength = 6f;
+    public float slideBoost = 4f;
+    [SerializeField] float airMultiplier = 0.4f;
+
+    [Header("Keybinds")]
+    [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode crouchKey = KeyCode.LeftControl;
+    [SerializeField] KeyCode dashKey = KeyCode.LeftShift;
+
+    [Header("Jumping")]
+    public float jumpForce = 5f;
+
+    [Header("Drag")]
+    float groundDrag = 6f;
+    float airDrag = 2f;
+
+    float playerHeight = 2f;
+
+    
+
+
+    public float crouch = -1f;
+
+    float horizontalMovement;
+    float verticalMovemement;
+    
+
+    bool isGrounded;
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+    public BoxCollider hitbox;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
+
+        MyInput();
+        ControlDrag();
+
+        if (Input.GetKeyDown(jumpKey) && isGrounded)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(crouchKey))
+        {        
+            Crouch();
+        }
+        else if (Input.GetKeyUp(crouchKey) && isGrounded)
+        {
+            Stand();
+        }
+        else if (Input.GetKeyUp(crouchKey) && isGrounded == false)
+        {
+            Stand();
+        }
+
+        if (Input.GetKeyDown(dashKey))
+        {
+            Dash();
+        }
+    }
+
+    void MyInput()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovemement = Input.GetAxisRaw("Vertical");
+
+        moveDirection = transform.forward * verticalMovemement + transform.right * horizontalMovement;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void Crouch()
+    {       
+        if (rb.velocity.magnitude != 0 && isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * movespeed * slideBoost, ForceMode.Impulse);
+        }
+        m_Camera.transform.Translate(0,-0.5f, 0);
+
+        hitbox.center = new Vector3(0, -0.5f,0);
+        hitbox.size = new Vector3(0.75f, 1, 0.75f);
+    }
+    void Stand()
+    {
+        m_Camera.transform.Translate(0, 0.5f, 0);
+        hitbox.center = new Vector3(0, 0, 0);
+        hitbox.size = new Vector3(0.75f, 2, 0.75f);
+    }
+
+    void Dash()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * movespeed * dashstrength, ForceMode.VelocityChange);
+        }
+        else if (isGrounded == false)
+        {
+            rb.AddForce(moveDirection.normalized * movespeed * airMultiplier * dashstrength, ForceMode.VelocityChange);
+        }
+    }
+
+    void ControlDrag()
+    {
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * movespeed * movementMultiplier, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * movespeed * movementMultiplier * airMultiplier, ForceMode.Acceleration);
+        }
+        
+    }
+}
